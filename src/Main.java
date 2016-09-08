b5dc918094aea49ab147323575a378d4d5728760
Simon_Clark <simon_clark@whs0694.local>
2016-09-08 13:42:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by simon_clark on 9/8/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World from HACS");
+        System.out.println("Change v1");
+    }
+
+}
