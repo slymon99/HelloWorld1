b3a98696f70153ed8c8619829400e7fc42069240
simon_clark <simonclark236@gmail.com>
2016-09-08 14:02:21 -0400

    commit v2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World from HACS");
-        System.out.println("Change v1");
+        System.out.println("Change v2");
     }
 
 }
